fafe88e1a066252800e18e25f84383d0545e9083
Samuel Obeng <samuelobeng2006@gmail.com>
2021-04-27 17:20:20 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let searchinput = document.querySelector("#search-input");
+let searchicon = document.querySelector(".search-icon");
+
+searchicon.addEventListener("click", function () {
+  searchinput.style.display = "block";
+});
